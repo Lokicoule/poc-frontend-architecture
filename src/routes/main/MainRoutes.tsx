ccc0f0c6b66c0d65d398fb733d5d19f81002ff9b
lokicoule <lokicoule@yahoo.fr>
2022-03-08 15:53:11 +0100

    Add feature : not-found

---
+++ src/routes/main/MainRoutes.tsx
@@ -0,0 +1,12 @@
+import { Navigate, RouteObject } from "react-router-dom";
+import { MainLayout } from "../../layouts";
+import { NotFoundPage } from "../../pages/main";
+
+export const MainRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [{ path: "not-found", element: <NotFoundPage /> }],
+  },
+  { path: "*", element: <Navigate to="/not-found" replace /> },
+];
