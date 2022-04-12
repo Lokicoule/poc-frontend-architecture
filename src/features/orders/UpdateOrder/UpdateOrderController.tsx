50a9511934133f3e4788e3436504a6339c989c9d
lokicoule <lokicoule@yahoo.fr>
2022-04-12 15:22:23 +0200

    feat(orders)! : api updated

--- src/features/orders/UpdateOrder/UpdateOrderController.tsx
+++ src/features/orders/UpdateOrder/UpdateOrderController.tsx
@@ -1,13 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { useGetCustomersQuery } from "../../../api/fdo/operations/customers.generated";
 import {
   GetOrderQuery,
   useGetOrderQuery,
   useUpdateOrderMutation,
 } from "../../../api/fdo/operations/orders.generated";
+import { useGetProductsQuery } from "../../../api/fdo/operations/products.generated";
 import {
   CreateOrderInput,
+  OrderCustomerInput,
   OrderItemInput,
+  OrderProductInput,
 } from "../../../api/fdo/types/dto-types.generated";
 import { Loader } from "../../../components";
 import {
@@ -32,8 +36,39 @@ export const UpdateOrderController = ({
   const [updateOrder, { error }] = useUpdateOrderMutation({
     refetchQueries: ["GetOrder"],
   });
+  const { data: customers } = useGetCustomersQuery({
+    fetchPolicy: "cache-only",
+  });
+  const { data: products } = useGetProductsQuery({
+    fetchPolicy: "cache-only",
+  });
 
-  /* const mapDtoToViewModel = (
+  const productViewModelToDto = (productId: string): OrderProductInput => {
+    const product = products?.getProducts?.find(
+      (product) => product.id === productId
+    );
+    return {
+      id: product?.id || "",
+      code: product?.code || "",
+      label: product?.label || "",
+    };
+  };
+
+  const customerViewModelToDto = (customerId: string): OrderCustomerInput => {
+    const customer = customers?.getCustomers?.find(
+      (customer) => customer.id === customerId
+    );
+    return {
+      id: customer?.id || "",
+      address: customer?.address || "",
+      city: customer?.city || "",
+      code: customer?.code || "",
+      naming: customer?.naming || "",
+      zipCode: customer?.zipCode || "",
+    };
+  };
+
+  const mapDtoToViewModel = (
     dataDto: GetOrderQuery | undefined
   ): FormOrderViewModel => {
     const order = dataDto?.getOrder;
@@ -41,11 +76,11 @@ export const UpdateOrderController = ({
       code: order?.code || "",
       billingDate: new Date(order?.billingDate),
       dueDate: new Date(order?.dueDate),
-      customer: order?.customer.id,
+      customerId: order?.customer.id,
       items: order?.items?.map((item) => ({
         amount: item.amount,
         unitPrice: item.unitPrice,
-        product: item.product?.id,
+        productId: item.product?.id,
       })),
     } as FormOrderViewModel;
   };
@@ -57,14 +92,14 @@ export const UpdateOrderController = ({
       (item) =>
         ({
           amount: Number(item.amount),
-          product: item.product,
+          product: productViewModelToDto(item.productId),
           unitPrice: Number(item.unitPrice),
         } as OrderItemInput)
     );
 
   const mapViewModelToDto = (data: FormOrderViewModel): CreateOrderInput => ({
     code: data?.code,
-    customer: data.customer,
+    customer: customerViewModelToDto(data.customerId),
     billingDate: data?.billingDate,
     dueDate: data?.dueDate,
     items: mapItemsViewModelToDto(data?.items),
@@ -85,15 +120,14 @@ export const UpdateOrderController = ({
           `La modification du produit ${data?.getOrder?.code} a échouée.`
         ),
     });
-  }; */
+  };
 
   if (loading) return <Loader></Loader>;
   return (
-    <></>
-    /* <UpdateOrderLogic
+    <UpdateOrderLogic
       defaultValues={mapDtoToViewModel(data)}
       onSubmit={handleSubmit}
       errors={error?.graphQLErrors}
-    ></UpdateOrderLogic> */
+    ></UpdateOrderLogic>
   );
 };
